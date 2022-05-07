bc835657dd492b772863a5d6f85999d4bc3f8863
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-05-08 05:01:19 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -40,7 +40,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 CUSTOM_TEXT = (
     str(PMPERMIT_TEXT)
     if PMPERMIT_TEXT
-    else f"__Halo kawan, saya bot yang menjaga room chat Ciio-Userbot {DEFAULTUSER} di mohon jangan melakukan spam , kalau anda melakukan itu OTOMATIS saya akan memblockir anda!__ \n"
+    else f"__Halo kawan, saya bot yang menjaga room chat Superman-Userbot {DEFAULTUSER} di mohon jangan melakukan spam , kalau anda melakukan itu OTOMATIS saya akan memblockir anda!__ \n"
 )
 DEF_UNAPPROVED_MSG = (
     "╔═════════════════════╗\n"
@@ -49,8 +49,8 @@ DEF_UNAPPROVED_MSG = (
     "**TOLONG JANGAN MELAKUKAN SPAM CHAT KEPADA MAJIKAN SAYA** \n"
     f"**YA TOD KARENA SAYA AKAN OTOMATIS MEMBLOKIR KAMU, TUNGGU SAMPAI {DEFAULTUSER} MENERIMA PESAN KAMU** \n"
     "╔═════════════════════╗\n"
-    "│○›Support : 📣 @projectcio      \n"
-    f"│○›ᗷy : 🤖 𝐂𝐈𝐈𝐎 𝐔𝐒𝐄𝐑𝐁𝐎𝐓​🔥           \n"
+    "│○›Support : 📣 @supermansupportt      \n"
+    f"│○›ᗷy : 🤖 𝐒𝐔𝐏𝐄𝐑𝐌𝐀𝐍 𝐔𝐒𝐄𝐑𝐁𝐎𝐓​🔥           \n"
     "╚═════════════════════╝"
 )
 # =================================================================
